457949668ed8f2e862d8cbbbe41bde6c4a2a6034
Alexander Kallaway <kallaway@me.com>
2017-04-12 01:40:08 -0400

    make progress on the demo app

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -11,7 +11,7 @@ test('should show rentals as the home page', function (assert) {
 });
 
 test('should link to information about the company.', function (assert) {
-  visit('/about');
+  visit('/');
   click('a:contains("About")');
   andThen(function() {
     assert.equal(currentURL(), '/about', 'should navigate to about');
@@ -19,11 +19,19 @@ test('should link to information about the company.', function (assert) {
 });
 
 test('should link to contact information.', function (assert) {
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function() {
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
 
 });
 
 test('should list available rentals.', function (assert) {
-
+  visit('/');
+  andThen(function() {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
 });
 
 test('should filter the list of rentals by city.', function (assert) {
