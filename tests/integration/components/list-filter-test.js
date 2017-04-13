daff4704d939fe301da3cddd06cf016bfd80c2c0
Alexander Kallaway <kallaway@me.com>
2017-04-13 12:54:28 -0400

    fixed bugs, went to start utilities and services section

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -3,16 +3,15 @@ import hbs from 'htmlbars-inline-precompile';
 import wait from 'ember-test-helpers/wait';
 import RSVP from 'rsvp';
 
-const ITEMS = [{city: 'San Francisco'}, {city: 'Portland'}, {city: 'Seattle'}];
-const FILTERED_ITEMS = [{city: 'San Francisco'}];
-
-moduleForComponent('list-filter', 'Integration | Component | list filter', {
+moduleForComponent('list-filter', 'Integration | Component | filter listing', {
   integration: true
 });
 
-test('should initially load all listings', function(assert) {
-  // we want our actions to return promises,
-  //since they are potentially fetching data asynchronously
+const ITEMS = [{city: 'San Francisco'}, {city: 'Portland'}, {city: 'Seattle'}];
+const FILTERED_ITEMS = [{city: 'San Francisco'}];
+
+test('should initially load all listings', function (assert) {
+  // we want our actions to return promises, since they are potentially fetching data asynchronously
   this.on('filterByCity', (val) => {
     if (val === '') {
       return RSVP.resolve(ITEMS);
@@ -22,8 +21,7 @@ test('should initially load all listings', function(assert) {
   });
 
   // with an integration test,
-  // you can set up and use your component in the same way your application
-  // will use it.
+  // you can set up and use your component in the same way your application will use it.
   this.render(hbs`
     {{#list-filter filter=(action 'filterByCity') as |results|}}
       <ul>
@@ -40,9 +38,7 @@ test('should initially load all listings', function(assert) {
     assert.equal(this.$('.city').length, 3);
     assert.equal(this.$('.city').first().text().trim(), 'San Francisco');
   });
-
 });
-
 
 test('should update with matching listings', function (assert) {
   this.on('filterByCity', (val) => {
